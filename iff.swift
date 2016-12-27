f81ecd28e67263c22bc7c8a7c5c0b4acac94d3fe
Tomasz Gebarowski <tomasz.gebarowski@azetti.com>
2016-12-27 18:31:21 +0100

    iff: moved example to comment in header

--- iff.swift
+++ iff.swift
@@ -4,19 +4,22 @@
 // If and only if operator <->, trying to replicate Ruby postifx if.
 //
 // Tomasz Gebarowski <gebarowski@gmail.com>
+//
+// Example:
+//
+// func register() { print("Registered") }
+// func subscribe() { print("Subscribed") }
+//
+// var shouldRegister = true
+// var shouldSubscribe = true
+//
+// register() <-> shouldRegister
+// subscribe() <-> shouldSubscribe
+//
 
 infix operator <->
 
 func <-> (closure: @autoclosure ()-> Void, condition: Bool) {
     condition ? closure() : ()
 }
-
-func register() { print("Registered") }
-func subscribe() { print("Subscribed") }
-
-var shouldRegister = true
-var shouldSubscribe = true
-
-register() <-> shouldRegister
-subscribe() <-> shouldSubscribe
 
